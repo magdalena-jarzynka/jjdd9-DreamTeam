cf59bd29d983015c47cfebda0bfaf75098fa67b6
Magdalena Jarzynka <magdalena.jarzynka@gmail.com>
2020-04-20 20:04:08 +0200

    classes based on API created https://jira.is-academy.pl/browse/JJDD9DT-4

---
+++ src/main/java/com/infoshareacademy/objects/Author.java
@@ -0,0 +1,10 @@
+package com.infoshareacademy.objects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Author {
+    String firstName;
+    String lastName;
+    List<Book> bookList = new ArrayList<>();
+}
---
+++ src/main/java/com/infoshareacademy/objects/Book.java
@@ -0,0 +1,8 @@
+package com.infoshareacademy.objects;
+
+public class Book {
+    String title;
+    Author author;
+    String genre;
+    boolean hasAudio;
+}
