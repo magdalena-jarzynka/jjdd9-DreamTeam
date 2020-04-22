fddb9a1de6efcb87601174a300f4e315e16e1dfe
Magdalena Jarzynka <magdalena.jarzynka@gmail.com>
2020-04-22 16:54:26 +0200

    classes based on API created - getters and setters added https://jira.is-academy.pl/browse/JJDD9DT-4

--- src/main/java/com/infoshareacademy/objects/Author.java
+++ src/main/java/com/infoshareacademy/objects/Author.java
@@ -1,6 +1,13 @@
 package com.infoshareacademy.objects;
 
 public class Author {
-    String name;
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/infoshareacademy/objects/Genre.java
+++ src/main/java/com/infoshareacademy/objects/Genre.java
@@ -1,5 +1,13 @@
 package com.infoshareacademy.objects;
 
 public class Genre {
-    String name;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
