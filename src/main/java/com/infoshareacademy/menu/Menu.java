003faf63c3dfba97cb322a8cefb6a0f15e819cbc
Adrian Wilk <awilk2@gmail.com>
2020-04-24 17:10:03 +0200

    Move menu methods from Main class to Menu class.

--- src/main/java/com/infoshareacademy/menu/Menu.java
+++ src/main/java/com/infoshareacademy/menu/Menu.java
@@ -5,13 +5,140 @@ import com.infoshareacademy.menu.item.BookListMenu;
 import com.infoshareacademy.menu.item.MainMenu;
 import com.infoshareacademy.menu.item.ReservationMenu;
 import com.infoshareacademy.menu.item.SettingsMenu;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
 
 public class Menu {
     private static final String SHOW_MENU = "{}. {} \n";
     private static final String GO_BACK = "Wybierz 0 aby wrócić do głównego Menu. \n";
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
+    private static final String WRONG_NUMBER = "Proszę wpisać odpowiednią cyfrę.";
+    private static final Scanner scanner = new Scanner(System.in);
+    private int input;
+
+    public Menu() {
+    }
+
+    public void run() {
+        while (true) {
+            input = openMainMenu();
+            STDOUT.info("\n");
+            if (input == 1) {
+                chooseBooksMenu();
+            }
+
+            if (input == 2) {
+                chooseReservationMenu();
+            }
+
+            if (input == 3) {
+                chooseSettingsMenu();
+            }
+
+            if (input == 4) {
+                STDOUT.info("Program został wyłączony. Miłego dnia!");
+                break;
+            }
+
+        }
+
+    }
+
+    private void chooseBooksMenu() {
+        int bookChoice = openBooksMenu();
+        switch (bookChoice) {
+            case 1:
+                getBooksMenu(BookListMenu.BOOK_LIST);
+                break;
+            case 2:
+                getBooksMenu(BookListMenu.SEARCH);
+                break;
+            case 3:
+                getBooksMenu(BookListMenu.BOOK_DETAILS);
+                break;
+            default:
+                input = openMainMenu();
+                break;
+        }
+    }
+
+    private void chooseReservationMenu() {
+        int reservationChoice = openReservationMenu();
+        switch (reservationChoice) {
+            case 1:
+                getReservationMenu(ReservationMenu.NEW_RESERVATION);
+                break;
+            case 2:
+                getReservationMenu(ReservationMenu.CANCEL_RESERVATION);
+                break;
+            default:
+                input = openMainMenu();
+                break;
+        }
+    }
+
+    private void chooseSettingsMenu() {
+        int settingsChoice = openSettingsMenu();
+        switch (settingsChoice) {
+            case 1:
+                getSettingsMenu(SettingsMenu.CONFIGURATIONS);
+                break;
+            case 2:
+                getSettingsMenu(SettingsMenu.SORTING);
+                break;
+            default:
+                getSettingsMenu(SettingsMenu.FORMAT);
+                break;
+        }
+    }
+
+    private int openMainMenu() {
+        STDOUT.info("Witaj! Wybierz pozycję z Menu wpisując jej numer lub wciśnij ENTER by wyjść: \n");
+        showMainMenu();
+        String lineInput = scanner.nextLine();
+        if (NumberUtils.isCreatable(lineInput)) {
+            input = Integer.parseInt(lineInput);
+        } else {
+            STDOUT.info(WRONG_NUMBER);
+        }
+        return input;
+    }
+
+    private int openBooksMenu() {
+        showBooksMenu();
+        String lineInput = scanner.nextLine();
+        if (NumberUtils.isCreatable(lineInput)) {
+            input = Integer.parseInt(lineInput);
+        } else {
+            STDOUT.info(WRONG_NUMBER);
+        }
+        return input;
+    }
+
+    private int openReservationMenu() {
+        showReservationMenu();
+        String lineInput = scanner.nextLine();
+        if (NumberUtils.isCreatable(lineInput)) {
+            input = Integer.parseInt(lineInput);
+        } else {
+            STDOUT.info(WRONG_NUMBER);
+        }
+        return input;
+    }
+
+    private int openSettingsMenu() {
+        showSettingsMenu();
+        String lineInput = scanner.nextLine();
+        if (NumberUtils.isCreatable(lineInput)) {
+            input = Integer.parseInt(lineInput);
+        } else {
+            STDOUT.info(WRONG_NUMBER);
+        }
+        return input;
+    }
 
     public void getBooksMenu(BookListMenu bookChoice) {
         BookListService bookListService = new BookListService();
@@ -99,6 +226,7 @@ public class Menu {
         }
         STDOUT.info(GO_BACK);
     }
+
 
     public void cleanTerminal() {
         STDOUT.info("\033\143");
