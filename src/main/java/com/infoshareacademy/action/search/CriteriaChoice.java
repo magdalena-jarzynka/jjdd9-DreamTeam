781621a7ec83048b7f9a1eed62fde52090d56da2
Piotr Wawreniuk <piotr21wawreniuk@gmail.com>
2020-05-04 16:23:35 +0200

    Add curly brackets to if

--- src/main/java/com/infoshareacademy/action/search/CriteriaChoice.java
+++ src/main/java/com/infoshareacademy/action/search/CriteriaChoice.java
@@ -3,16 +3,12 @@ package com.infoshareacademy.action.search;
 import com.infoshareacademy.input.UserInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.awt.*;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class CriteriaChoice {
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
     private static final Scanner scanner = new Scanner(System.in);
-    private boolean[] activeCriteria = new boolean[3];
+    private final boolean[] activeCriteria = new boolean[3];
     private String author = "";
     private String title = "";
     private boolean audio = false;
--- src/main/java/com/infoshareacademy/action/search/Filtration.java
+++ src/main/java/com/infoshareacademy/action/search/Filtration.java
@@ -1,41 +1,37 @@
 package com.infoshareacademy.action.search;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.infoshareacademy.action.BookListService;
-import com.infoshareacademy.input.UserInput;
 import com.infoshareacademy.object.Book;
-import com.infoshareacademy.repository.BookRepository;
 import com.infoshareacademy.service.BookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filtration {
-    private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
 
     public static void run(CriteriaChoice userCriteria) {
         BookService bookService = new BookService();
         Map<Long, Book> filteredBooks = bookService.findAllBooks();
 
-        if(userCriteria.getActiveCriteria()[0])
+        if (userCriteria.getActiveCriteria()[0]) {
             filteredBooks = filteredBooks.entrySet().stream()
                     .filter(x -> x.getValue().getTitle().toLowerCase().
                             contains(userCriteria.getTitle()))
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-        if(userCriteria.getActiveCriteria()[1])
-        filteredBooks = filteredBooks.entrySet().stream()
-                .filter(x -> x.getValue().getAuthors().get(0).getName().toLowerCase().
-                        contains(userCriteria.getAuthor()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-        if(userCriteria.getActiveCriteria()[2])
+        }
+        if (userCriteria.getActiveCriteria()[1]) {
+            filteredBooks = filteredBooks.entrySet().stream()
+                    .filter(x -> x.getValue().getAuthors().get(0).getName().toLowerCase().
+                            contains(userCriteria.getAuthor()))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        }
+        if (userCriteria.getActiveCriteria()[2]) {
             filteredBooks = filteredBooks.entrySet().stream()
                     .filter(x -> x.getValue().hasAudio() == userCriteria.getAudio())
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
+        }
         BookListService filteredList = new BookListService();
         filteredList.run(filteredBooks);
     }
