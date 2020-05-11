f6c73666073b502c32f16a05b6f9f82f9bd851ba
Piotr Wawreniuk <piotr21wawreniuk@gmail.com>
2020-05-11 16:52:19 +0200

    Fix sorting when polish characters occurs

--- src/main/java/com/infoshareacademy/comparator/SortingByTitle.java
+++ src/main/java/com/infoshareacademy/comparator/SortingByTitle.java
@@ -2,15 +2,18 @@ package com.infoshareacademy.comparator;
 
 import com.infoshareacademy.object.Book;
 
+import java.text.Collator;
 import java.util.Comparator;
+import java.util.Locale;
 import java.util.Map;
 
 public class SortingByTitle implements Comparator<Map.Entry<Long, Book>> {
 
     @Override
     public int compare(Map.Entry<Long, Book> book1, Map.Entry<Long, Book> book2) {
+        Collator c = Collator.getInstance(new Locale("pl", "PL"));
         String title1 = book1.getValue().getTitle().toLowerCase();
         String title2 = book2.getValue().getTitle().toLowerCase();
-        return title1.compareTo(title2);
+        return c.compare(title1, title2);
     }
 }
