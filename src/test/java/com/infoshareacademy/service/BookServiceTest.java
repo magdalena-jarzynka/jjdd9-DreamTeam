1043a39eeee7deda66f7a825d0e74955d612d90f
Adrian Wilk <awilk2@gmail.com>
2020-05-11 02:26:09 +0200

    Add new test before merging develop

---
+++ src/test/java/com/infoshareacademy/service/BookServiceTest.java
@@ -0,0 +1,45 @@
+package com.infoshareacademy.service;
+
+import com.infoshareacademy.object.Book;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+class BookServiceTest {
+
+    @Test
+    void name() {
+        //GIVEN
+        BookService bookService = new BookService();
+        Book book = new Book();
+        book.setId(1L);
+
+        String result;
+
+        //WHEN
+        result = bookService.getBookDetails(book.getId());
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isNotEmpty()
+                .contains("Wiersz")
+                .contains("Tadeusz Miciński")
+                .doesNotContain("Jan Kochanowski");
+
+    }
+
+    @Test
+    void checkIfContainsNullOrEmpty() {
+        //GIVEN
+        BookService bookService = new BookService();
+        Map<Long, Book> result;
+
+        //WHEN
+        result = bookService.findAllBooks();
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isNotEmpty()
+                .isNotNull();
+
+    }
+
+}
