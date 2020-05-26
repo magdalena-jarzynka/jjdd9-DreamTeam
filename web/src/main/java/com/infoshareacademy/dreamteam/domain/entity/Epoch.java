eeea5d3b4c2ca3b547d212318a1fd88d234cc813
Piotr Wawreniuk <piotr21wawreniuk@gmail.com>
2020-05-26 17:00:33 +0200

    Create entities

---
+++ web/src/main/java/com/infoshareacademy/dreamteam/domain/entity/Epoch.java
@@ -0,0 +1,47 @@
+package com.infoshareacademy.dreamteam.domain.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "epoch")
+public class Epoch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "book_epoch",
+            joinColumns = @JoinColumn(name = "epoch_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"))
+    List<Book> books = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
