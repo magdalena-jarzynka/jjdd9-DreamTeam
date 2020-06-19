2b495fd559e314b2e58eb1aea077660ba0521f5f
Piotr Wawreniuk <piotr21wawreniuk@gmail.com>
2020-06-19 19:23:12 +0200

    Create method sending emails after deleting book from the database

--- web/src/main/java/com/infoshareacademy/dreamteam/service/FavouritesService.java
+++ web/src/main/java/com/infoshareacademy/dreamteam/service/FavouritesService.java
@@ -2,30 +2,39 @@ package com.infoshareacademy.dreamteam.service;
 
 import com.infoshareacademy.dreamteam.domain.entity.Book;
 import com.infoshareacademy.dreamteam.domain.entity.User;
+import com.infoshareacademy.dreamteam.email.BookReservationEmailBuilder;
+import com.infoshareacademy.dreamteam.email.EmailManager;
+import com.infoshareacademy.dreamteam.email.FavouriteBookEmailBuilder;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Stateless
 public class FavouritesService {
 
     @EJB
-    UserService userservice;
+    UserService userService;
 
     @EJB
     BookService bookService;
 
+    @Inject
+    private EmailManager emailManager;
+
     @Transactional
     public boolean addBookToFavourites(Long bookId, Long userId) {
-        User user = userservice.findUserById(userId);
+        User user = userService.findUserById(userId);
         Book book = bookService.findById(bookId);
         if(user.getFavourites().contains(book)) {
             return false;
         } else {
             user.getFavourites().add(book);
             book.getFavourites().add(user);
-            userservice.update(user);
+            userService.update(user);
             bookService.update(book);
             return true;
         }
@@ -33,16 +42,29 @@ public class FavouritesService {
 
     @Transactional
     public boolean removeBookFromFavourites(Long bookId, Long userId) {
-        User user = userservice.findUserById(userId);
+        User user = userService.findUserById(userId);
         Book book = bookService.findById(bookId);
         if(user.getFavourites().contains(book)) {
             user.getFavourites().remove(book);
             book.getFavourites().remove(user);
-            userservice.update(user);
+            userService.update(user);
             bookService.update(book);
             return true;
         } else {
             return false;
+        }
+    }
+
+    @Transactional
+    public void favouriteRemoval(Long bookId) {
+        Book book = bookService.findById(bookId);
+        for(User user: userService.findAll()) {
+            List<Long> favouritesId = userService.getFavourites(user.getId()).stream()
+                    .map(Book::getId).collect(Collectors.toList());
+            if(favouritesId.contains(bookId)) {
+                removeBookFromFavourites(bookId, user.getId());
+                emailManager.sendEmail(new FavouriteBookEmailBuilder(book.getTitle()), user.getEmail());
+            }
         }
     }
 }
