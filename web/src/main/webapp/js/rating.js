4edb0182ba6a96f22c4d9d6266202846b8490481
Adrian Wilk <awilk2@gmail.com>
2020-06-19 21:13:59 +0200

    ..

--- web/src/main/webapp/js/rating.js
+++ web/src/main/webapp/js/rating.js
@@ -1,61 +1,38 @@
-$(document).ready(function() {
-
-    $('.rate_widget').each(function(i) {
-        var widget = this;
-        var out_data = {
-            widget_id : $(widget).attr('id'),
-            fetch: 1
-        };
-        $.post(
-            'ratings.php',
-            out_data,
-            function(INFO) {
-                $(widget).data( 'fsr', INFO );
-                set_votes(widget);
-            },
-            'json'
-        );
-    });
+$('.ratings_stars').hover(
+    // Handles the mouseover
+    function () {
+        $(this).prevAll().andSelf().addClass('ratings_over');
+        $(this).nextAll().removeClass('ratings_vote');
+    },
+    // Handles the mouseout
+    function () {
+        $(this).prevAll().andSelf().removeClass('ratings_over');
+        // can't use 'this' because it wont contain the updated data
+        set_votes($(this).parent());
+    }
+);
 
 
-    $('.ratings_stars').hover(
-        // Handles the mouseover
-        function() {
-            $(this).prevAll().andSelf().addClass('ratings_over');
-            $(this).nextAll().removeClass('ratings_vote');
+// This actually records the vote
+$('.ratings_stars').bind('click', function () {
+    var star = this;
+    var widget = $(this).parent();
+
+    var clicked_data = {
+        clicked_on: $(star).attr('class'),
+        widget_id: $(star).parent().attr('id')
+    };
+    $.post(
+        'ratings.php',
+        clicked_data,
+        function (INFO) {
+            widget.data('fsr', INFO);
+            set_votes(widget);
         },
-        // Handles the mouseout
-        function() {
-            $(this).prevAll().andSelf().removeClass('ratings_over');
-            // can't use 'this' because it wont contain the updated data
-            set_votes($(this).parent());
-        }
+        'json'
     );
-
-
-    // This actually records the vote
-    $('.ratings_stars').bind('click', function() {
-        var star = this;
-        var widget = $(this).parent();
-
-        var clicked_data = {
-            clicked_on : $(star).attr('class'),
-            widget_id : $(star).parent().attr('id')
-        };
-        $.post(
-            'ratings.php',
-            clicked_data,
-            function(INFO) {
-                widget.data( 'fsr', INFO );
-                set_votes(widget);
-            },
-            'json'
-        );
-    });
-
-
-
 });
+
 
 function set_votes(widget) {
 
@@ -67,5 +44,5 @@ function set_votes(widget) {
 
     $(widget).find('.star_' + avg).prevAll().andSelf().addClass('ratings_vote');
     $(widget).find('.star_' + avg).nextAll().removeClass('ratings_vote');
-    $(widget).find('.total_votes').text( votes + ' votes recorded (' + exact + ' rating)' );
+    $(widget).find('.total_votes').text(votes + ' votes recorded (' + exact + ' rating)');
 }
