b99a5a0799aec2267ee26e9889e17babb249cba8
Adrian Wilk <awilk2@gmail.com>
2020-06-22 03:56:13 +0200

    Fix second admin account

--- web/src/main/resources/start.sql
+++ web/src/main/resources/start.sql
@@ -3,4 +3,4 @@ INSERT INTO user_type (name) VALUE ('USER');
 INSERT INTO user_type (name) VALUE ('ADMIN');
 INSERT INTO user_type (name) VALUE ('SUPER_ADMIN');
 INSERT INTO user (email, name, role_id) VALUES ('jjdd9dreamteam@gmail.com', 'Dream Team', 4);
-INSERT INTO user (email, name, role_id) VALUES ('jjdd9dtmail.com', 'Dream Team2', 4);
+INSERT INTO user (email, name, role_id) VALUES ('jjdd9dtmail@gmail.com', 'Dream Team2', 4);
